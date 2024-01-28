827dff93b42a1bf1f023e7f4e438fb56ff3eec3c
NDNahuel Duca <nahuel2005duca@gmail.com>
2024-01-27 23:45:12 -0300

    Ejercicios del modulo 5

---
+++ ejercicios/20.js
@@ -0,0 +1,15 @@
+function esVocal(letra) {
+  // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+  // Si el usuario ingresa un string de más de un caracter, debes retornar el mensaje: "Dato incorrecto".
+  // Si no es vocal, tambien debe retornar "Dato incorrecto".
+  // Tu código:
+  if (typeof letra === "string" && letra.length === 1) 
+  if ("aeiouAEIOU".indexOf(letra) !== -1) {
+    return "Es vocal";
+  } else {
+    return "Dato incorrecto";
+  } else {
+  return "Dato incorrecto";
+}
+}
+module.exports = esVocal;
---
+++ ejercicios/24.js
@@ -0,0 +1,12 @@
+function divide(x, y) {
+  // La función recibe dos argumentos "x" e "y" que son números.
+  // Retorna el resultado de su división.
+  // Tu código:
+  if (y !== 0) {
+    return x / y;
+  } else {
+    return "Error: División por cero";
+  }
+}
+
+module.exports = divide;
